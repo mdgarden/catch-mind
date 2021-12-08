61c7539435a505b2b038262ed10de80ad4c7dabb
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-12-09 00:14:09 +0900

    Print out Hello World

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 
 app.use(express.static(join(__dirname, "static")));
 
@@ -17,3 +19,9 @@ const handleListening = () =>
 const server = app.listen(PORT, handleListening);
 
 const io = socketIO(server);
+
+let sockets = [];
+
+io.on("connection", (socket) => {
+  sockets.push(socket.id);
+});
