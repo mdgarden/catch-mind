42be09eb56abfcc18315ed4567e56915dbf377a0
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-12-14 22:25:35 +0900

    setting gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,19 @@
+import gulp from "gulp";
+import gulpSass from "gulp-sass";
+import nodeSass from "node-sass";
+
+const sass = gulpSass(nodeSass);
+
+const paths = {
+  styles: {
+    src: "assets/scss/styles.scss",
+    dest: "src/static/styles",
+  },
+};
+
+export function styles() {
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sass())
+    .pipe(gulp.dest(paths.styles.dest));
+}
