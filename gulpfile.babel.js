9ec517c233bba50377d8bcd91cc38b102e3eda6b
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-12-15 23:04:08 +0900

    Finish Gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,8 @@
 import gulp from "gulp";
 import gulpSass from "gulp-sass";
 import nodeSass from "node-sass";
+import autoPrefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
 
 const sass = gulpSass(nodeSass);
 
@@ -8,12 +10,27 @@ const paths = {
   styles: {
     src: "assets/scss/styles.scss",
     dest: "src/static/styles",
+    watch: "assets/scss/**/*.scss",
   },
 };
 
-export function styles() {
+function styles() {
   return gulp
     .src(paths.styles.src)
     .pipe(sass())
+    .pipe(
+      autoPrefixer({
+        cascade: false,
+      })
+    )
+    .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest));
 }
+
+function watchFiles() {
+  gulp.watch(paths.styles.watch, styles);
+}
+
+const dev = gulp.series([styles, watchFiles]);
+
+export default dev;
