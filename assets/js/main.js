1b2668ff8355b3c174cc8c3b846567474c4887ce
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-12-16 23:54:48 +0900

    Setting Browerify

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,13 @@
-//eslint-disable-next-line no-undef
+import { handleMessageNotif } from "./chat";
+
 const socket = io("/");
 
 function sendMessage(message) {
   socket.emit("newMessage", { message });
   console.log(`You: ${message}`);
 }
-
 function setNickname(nickname) {
   socket.emit("setNickname", { nickname });
-}
-
-function handleMessageNotif(data) {
-  const { message, nickname } = data;
-  console.log(`${nickname}: ${message}`);
 }
 
 socket.on("messageNotif", handleMessageNotif);
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,8 @@ import gulpSass from "gulp-sass";
 import nodeSass from "node-sass";
 import autoPrefixer from "gulp-autoprefixer";
 import minifyCSS from "gulp-csso";
+import del from "del";
+import bro from "gulp-browserify";
 
 const sass = gulpSass(nodeSass);
 
@@ -12,10 +14,17 @@ const paths = {
     dest: "src/static/styles",
     watch: "assets/scss/**/*.scss",
   },
+  js: {
+    src: "assets/js/main.js",
+    dest: "src/static/js",
+    watch: "assets/js/**/*.js",
+  },
 };
 
-function styles() {
-  return gulp
+const clean = () => del(["src/static"]);
+
+const styles = () =>
+  gulp
     .src(paths.styles.src)
     .pipe(sass())
     .pipe(
@@ -25,12 +34,12 @@ function styles() {
     )
     .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest));
-}
 
-function watchFiles() {
-  gulp.watch(paths.styles.watch, styles);
-}
+const js = () =>
+  gulp.src(paths.js.src).pipe(bro()).pipe(gulp.dest(paths.js.dest));
 
-const dev = gulp.series([styles, watchFiles]);
+const watchFiles = () => gulp.watch(paths.styles.watch, styles);
+
+const dev = gulp.series(clean, styles, js, watchFiles);
 
 export default dev;
